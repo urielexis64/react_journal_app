29788f1cd5c706d810103b9d99fabe644a082bf9
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-15 22:52:55 -0600

    config main routes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Route, BrowserRouter as Router, Switch, Redirect} from "react-router-dom";
+import {JournalScreen} from "../components/journal/JournalScreen";
+import {AuthRouter} from "./AuthRouter";
+
+export const AppRouter = () => {
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route path='/auth' component={AuthRouter} />
+					<Route exact path='/' component={JournalScreen} />
+
+					<Redirect to='/auth/login' />
+				</Switch>
+			</div>
+		</Router>
+	);
+};
---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Redirect, Route, Switch} from "react-router";
+import {LoginScreen} from "../components/auth/LoginScreen";
+import {RegisterScreen} from "../components/auth/RegisterScreen";
+
+export const AuthRouter = () => {
+	return (
+		<div>
+			<Switch>
+				<Route path='/auth/login' component={LoginScreen} />
+				<Route path='/auth/register' component={RegisterScreen} />
+				<Redirect to='/auth/login' />
+			</Switch>
+		</div>
+	);
+};
