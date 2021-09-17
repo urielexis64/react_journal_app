f367f4b029fbc4b9be8cf83f2736d9f43524c7bb
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-16 19:23:54 -0600

    create NothingSelected component

---
+++ src/components/journal/NothingSelected.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NothingSelected = () => {
+	return (
+		<div className='nothing_main-content'>
+			<p>
+				Select something
+				<br />
+				or create a new entry!
+			</p>
+			<i className='far fa-star fa-4x mt-5'></i>
+		</div>
+	);
+};
