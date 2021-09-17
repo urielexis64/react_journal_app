5af0a8483abe16bfdaf10e541e37800c19238841
Uriel Aispuro <urielalexis64@gmail.com>
2021-09-16 20:25:00 -0600

    add NoteScreen form

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -3,8 +3,24 @@ import {NotesAppBar} from "./NotesAppBar";
 
 export const NoteScreen = () => {
 	return (
-		<div className='notes_main-content'>
+		<div className='notes__main-content'>
 			<NotesAppBar />
+			<div className='notes__content'>
+				<input
+					autoComplete='off'
+					type='text'
+					placeholder='Some title'
+					className='notes__title-input'
+				/>
+				<textarea placeholder='What happened today?' className='notes__textarea'></textarea>
+
+				<div className='notes__image'>
+					<img
+						src='https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__340.jpg'
+						alt='image'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
